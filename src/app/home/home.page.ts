54821a6d98ed1d692fb7a63575e7da0f39d9bf07
Michael <michael@codetropolis.com>
2020-01-08 14:42:14 -0600

    Draggable events wip.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
+import interactionPlugin,  { Draggable } from '@fullcalendar/interaction';
 import { FirebaseService } from '../services/firebase.service';
 import * as moment from 'moment';
 import { ModalController } from '@ionic/angular';
 import { AddEventPage } from '../modals/add-event/add-event.page';
 import { EventInfoPage } from '../modals/event-info/event-info.page';
+import { FullCalendarComponent } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,11 @@ import { EventInfoPage } from '../modals/event-info/event-info.page';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+
+  @ViewChild(FullCalendarComponent, {static: true}) cal: FullCalendarComponent;
+  @ViewChild(Element, {static: true}) draggable: Element;
+  @ViewChild('external',  {static: true}) external: ElementRef;
+
 
   calendarPlugins = [
     dayGridPlugin,
@@ -24,9 +30,20 @@ export class HomePage implements OnInit {
   constructor(private firebaseService: FirebaseService,  private modalController: ModalController) {}
 
   ngOnInit() {
+    // console.log(`MD: HomePage -> cal`, this.cal);
     this.firebaseService.events.subscribe(events => {
       this.events = events;
     });
+    // tslint:disable-next-line: no-unused-expression
+    new Draggable(this.external.nativeElement, {
+      itemSelector: '.fc-event',
+      eventData: (eventEl) => {
+        // console.log(`MD: HomePage -> ngOnInit -> eventEl`, eventEl);
+        return {
+          title: eventEl.innerText
+        };
+      }
+  });
   }
 
   dateClick(arg) {
